e8785eeaaa7806a7939e1439db27a8f6857e3fc1
yisus <jesusmr92@gmail.com>
2018-07-19 23:56:45 +0200

    Inicio proyecto Nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '51.68.71.2';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
